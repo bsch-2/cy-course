a8c19dc462ad2961631fffda9d29655922f9e137
Bartłomiej Schneider <bartlomiej.schneider@itmagination.com>
2022-03-29 17:37:34 +0200

    first test

---
+++ cypress/integration/0-webdriveruniversity-tests/contact_us_form.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe('\"Contact Us\" form testing', () => {
+
+    it('', () => {
+        cy.visit('/Contact-Us/contactus.html');
+        cy.get('input');
+        cy.get('#form_buttons');
+        cy.get('.feedback-input');
+        cy.get('[type]');
+        cy.get('[name="first_name"]');
+        cy.get('[class="feedback-input"]');
+    })
+
+})
