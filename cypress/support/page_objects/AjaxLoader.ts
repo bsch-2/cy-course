f37351c24b266f96d0007e51f2ca201d3080fcaa
Bartłomiej Schneider <bartlomiej.schneider@itmagination.com>
2022-04-08 13:49:18 +0200

    Removed unused npm package

--- cypress/support/page_objects/AjaxLoader.ts
+++ cypress/support/page_objects/AjaxLoader.ts
@@ -1,31 +1,11 @@
 /// <reference types="cypress" />
 
 export class AjaxLoader {
-    // myDiv: string = Cypress.$('#myDiv').attr('style');
-    // text: string = "nothing"
     clickMeButton: string = '#myDiv';
 
     clickMeButtonWait() {
         cy.get(this.clickMeButton).click({timeout: 10000});
     }
-
-    // clickMeButton() {
-    //     cy.log(this.text)
-    //     cy.get('#myDiv').invoke('attr', 'style')
-    //     expect(this.myDiv).to.be.equal("")
-    //     cy.log(this.text)
-    //     // cy.waitUntil(() => ())
-    // }
 }
 
 export const ajaxLoader = new AjaxLoader();
-
-// it('Should detect the DOM change with cy.waitUntil', () => {
-//     cy.contains('Mutate after a random delay').click()
-//     cy.waitUntil(() => (mutationsHistory.length ? mutationsHistory : false)).then(
-//         (mutationsHistory) => {
-//             cy.log('Now you can do whatever you want with `mutationsHistory`')
-//             console.log(mutationsHistory)
-//         }
-//     )
-// })
